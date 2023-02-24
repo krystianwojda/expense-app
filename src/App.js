550657eec057a101a4c1ae6097455050d1d5204c
Krystian <krystian.wojda-98@wp.pl>
2023-02-24 12:00:16 +0100

    Create fake expenses

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 import Expenses from "./components/Expenses/Expenses";
 
+const dummy_expenses = [
+    {
+        id: 1,
+        title: 'Car Insurance',
+        amount: 499.00,
+        date: new Date(2022, 9,25)
+    },
+    {
+        id: 2,
+        title: 'New Laptop',
+        amount: 1299,
+        date: new Date(2022, 7, 7)
+    },
+    {
+        id: 3,
+        title: 'Iphone 12 Pro',
+        amount: 899.99,
+        date: new Date(2022, 12,24)
+    },
+    {
+        id: 4,
+        title: 'Michelin tyres',
+        amount: 500,
+        date: new Date(2023,1,14)
+    }
+];
+
 const App = () => {
     return(
         <div>
