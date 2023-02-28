abd5c9f76c82f5cf4a69026e5eb4f4c5f3dbd575
Krystian <krystian.wojda-98@wp.pl>
2023-02-28 10:01:20 +0100

    Configuration fills

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,7 +4,9 @@ import './Chart.css';
 const Chart = (props) => {
     return(
         <div className='chart'>
-            {props.dataPoints.map(dataPoint => <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={null} label={dataPoint.label}/>)}
+            {props.dataPoints.map(dataPoint =>
+                <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={null} label={dataPoint.label}/>
+            )}
         </div>
     );
 };
